821c5ac35cf25fd02645ace0c435106668050bb6
Manfred Bergmann <mb@software-by-mabe.com>
2020-02-28 22:32:47 +0100

    added handle-call and call instead receive-fun and send. more leaned onto Erlangs GenServer.

--- src/actor.lisp
+++ src/actor.lisp
@@ -1,93 +1,103 @@
-(defpackage :cl-actors2
+(defpackage :cl-gserver
   (:use :cl :lparallel :lparallel.queue :log4cl)
   (:export #:set-threadpool-size
            #:set-receive-fun
            #:send))
 
-(in-package :cl-actors2)
+(in-package :cl-gserver)
 
-(defun set-threadpool-size (nthreads)
-  (setf *kernel* (make-kernel nthreads)))
+(defun init-threadpool (size)
+  (setf *kernel* (make-kernel size)))
 
 (defstruct actor-state (running t :type boolean))
 
-(defclass actor()
+(defclass gserver()
   ((name :initarg :name
-         :initform (concatenate 'string "Actor-" (princ-to-string (random 100000)))
+         :initform (concatenate 'string "Server-" (princ-to-string (random 100000)))
          :accessor name
          :documentation "Well, the name of the actor. If no name is specified a default one is applied.")
    (mailbox :initform (make-channel)
-            :accessor mailbox)
-   (receive-fun :initarg :receive-fun
-                :initform (error ":receive-fun must be specified!")
-                :accessor receive-fun
-                :documentation
-                "This is the message handler function. It takes one parameter that is the received message.")
+            :accessor mailbox
+            :documentation "The channel for submitting calls.")
    (internal-state :initarg :internal-state
                    :initform (make-actor-state)
                    :documentation "The internal state of the actor.")
    ))
 
-(defmethod initialize-instance :after ((self actor) &key)
+(defmethod initialize-instance :after ((self gserver) &key)
   :documentation "Not sure yet what this does.")
 
 ;; public functions
 
-(defun set-receive-fun (actor new-receive-fun)
-  "Set the receive handler function on an actor."
-  (when new-receive-fun
-    (with-slots (receive-fun) actor
-      (setf receive-fun new-receive-fun))))
+(defgeneric handle-call (gserver message)
+  (:documentation
+  "Handles calls to the server. Must be implemented by subclasses.
+A result can be returned which is forwarded to the caller."))
 
-(defun send (actor message)
-  "Send a message to an actor"
+(defun call (gserver message)
+  "Send a message to a gserver instance."
   (when message
     (log:debug "pushing ~a to mailbox" message)
-    (let ((result (submit-to-mailbox actor message)))
-      (log:debug "Message process result:" result))))
+    (let ((result (submit-to-mailbox gserver message)))
+      (log:debug "Message process result:" result)
+      result)))
 
 ;; internal functions
 
-(defun submit-to-mailbox (actor message)
+(defun submit-to-mailbox (gserver message)
   "Pushes the message to the mailbox channel"
-  (let ((*task-category* (concatenate 'string (name actor) "-task")))
-    (submit-task (mailbox actor) (lambda () (process-message actor (unwrap-message message))))
-    (receive-result (mailbox actor))))
+  (let ((*task-category* (concatenate 'string (name gserver) "-task")))
+    (submit-task (mailbox gserver) (lambda () (process-message gserver message)))
+    (receive-result (mailbox gserver))))
 
-(defun process-message (actor message)
+(defun process-message (gserver message)
   (log:debug "Handling message: " message)
   (when message
-    (handler-case (progn
-                    ;; First process internally
-                    (when (not (internal-receive-fun message))
-                      ;; then externally
-                      (let ((user-receive-fun-result (funcall (receive-fun actor) message)))
-                        (cond
-                          (user-receive-fun-result (log:debug "Message handled by user."))
-                          (t (log:debug "Message not handled."))))))
-      (t (c) (log:warn "Error condition was raised on message processing: " c)
+    (handler-case
+        (progn
+          ;; First process internally
+          (unless (handle-call-internal message)
+            ;; then externally
+            (let ((handle-call-result (handle-call gserver message)))
+              (cond
+                (handle-call-result
+                 (progn
+                   (log:debug "Message handled by handle-call.")
+                   handle-call-result))
+                (t
+                 (progn
+                   (log:debug "Message not handled.")
+                   nil))))))
+      (t (c)
+        (log:warn "Error condition was raised on message processing: " c)
         nil))))
 
-(defun internal-receive-fun (msg)
-  (log:debug "Internal receive: " msg)
+(defun handle-call-internal (msg)
+  (log:debug "Internal handle-call: " msg)
   nil)
 
-;; test receive handler
-(defun test-receive (msg)
-  (log:debug "received: " msg)
+
+;; dummy test gserver
+(defclass mygserver (gserver) ())
+(defmethod handle-call ((self mygserver) message)
+  (log:debug "handle-call from mygserver: " message)
   (cond
-    ((equal msg "Foo") 
+    ((equal message "Foo") 
      (progn
+       (sleep 3)
        (log:debug "Message handled: Foo")
        t))
     (t nil)))
 
-(defun error-receive (msg)
-  (throw 'foo 1))
+;; test receive handler
+
+;;(defun error-receive (msg)
+;;  (throw 'foo 1))
 
 ;; TODO:
 ;; OK - do loop while, until 'stop-condition
 ;; OK - add internal state for if we are running or not. When STOP was sent we should go to stopped state.
-;; - add actor mgr that can spawn new actors.
+;; - how to let 'destroy' a channel for cleanup?
 ;; - add state
+;; - add gserver mgr that can spawn new actors.
 ;; - add error handling

85a3d9245832d47d3caa6b94b43434beb1413830
Manfred Bergmann <mb@software-by-mabe.com>
2020-11-25 21:29:46 +0100

    adding routee.

--- src/router.lisp
+++ src/router.lisp
@@ -2,7 +2,9 @@
   (:use :cl)
   (:nicknames :router)
   (:export #:router
-           #:make-router)
+           #:make-router
+           #:add-routee
+           #:routees)
   )
 
 (in-package :cl-gserver.router)
@@ -11,8 +13,16 @@
   "Default constructor of router."
   (make-instance 'router))
 
-(defclass router () ()
+(defclass router ()
+  ((routees :initform (make-array 2 :adjustable t :fill-pointer 0)
+            :reader routees
+            :documentation "The routees."))
   (:documentation
    "A router combines a number of actors and implements the actor-api protocol.
+So a `tell', `ask' and `async-ask' is delegated to one of the routerss routees.
 A router strategy defines how one of the actors is determined as the forwarding target of the message."))
 
+(defun add-routee (router routee)
+  "Adds a routee/actor to the router."
+  (vector-push-extend routee (routees router))
+  routee)
--- tests/router-test.lisp
+++ tests/router-test.lisp
@@ -1,5 +1,5 @@
 (defpackage :cl-gserver.router-test
-  (:use :cl :fiveam :cl-gserver.router)
+  (:use :cl :fiveam :cl-mock :cl-gserver.router)
   (:export #:run!
            #:all-tests
            #:nil))
@@ -11,8 +11,23 @@
 
 (in-suite router-tests)
 
-(test create-router
+(test router--create
   "Creates a plain router"
   (is (not (null (make-router))))
-  (is (typep (make-router) 'router))
-  )
+  (is (typep (make-router) 'router)))
+
+(defparameter *fake-context* "fake-context")
+
+(test router--add-routee
+  "Tests adding routees (actors)"
+  (with-mocks ()
+    (answer (ac:actor-of _ create-fun) (funcall create-fun))
+    
+    (let ((cut (make-router)))
+      (dotimes (i 2)
+        (add-routee cut (ac:actor-of *fake-context*
+                                     (lambda ()
+                                       (act:make-actor (lambda (self msg state)
+                                                         (declare (ignore self))
+                                                         (cons msg state)))))))
+      (is (= 2 (length (routees cut)))))))

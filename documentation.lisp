b8a927e620a9863df1e54130f4f045f0033025d1
Manfred Bergmann <mb@software-by-mabe.com>
2021-03-04 22:22:34 +0100

    more mgl-pax docs.

--- documentation.lisp
+++ documentation.lisp
@@ -1,27 +1,12 @@
 
-(defpackage :cl-gserver.docs)
-(in-package :cl-gserver.docs)
-
-(pax:defsection @cl-gserver (:title "cl-gserver documentation")
-  "The main documentation of cl-gserver"
-
-  (asys:@actor-system pax:section)
-  (ac:@actor-context pax:section)
-  (act:@actor pax:section)
-  (disp:@dispatcher pax:section)
-  (router:@router pax:section)
-  )
-
 (in-package :asys)
 (pax:defsection @actor-system (:title "Actor-System")
-  "Description of Actor-System."
   (asys:actor-system class)
   ;;(asys:dispatchers (pax:reader actor-system))
   (asys:make-actor-system function))
 
 (in-package :ac)
 (pax:defsection @actor-context (:title "Actor-Context")
-  "Description of Actor-Context."
   (ac:actor-context class)
   (ac:make-actor-context function)
   (ac:actor-of generic-function)
@@ -35,9 +20,25 @@
   (ac:id generic-function)
   (ac:actor-name-exists condition))
 
+(in-package :act-cell)
+(pax:defsection @actor-cell (:title "Actor-Cell")
+  (act-cell:actor-cell class)
+  (act-cell:name (pax:reader actor-cell))
+  (act-cell:state (pax:reader actor-cell))
+  (act-cell:msgbox (pax:accessor actor-cell))
+  (act-cell:msgbox (pax:accessor actor-cell))
+  (act-cell:*sender* variable)
+  (act-cell:handle-call generic-function)
+  (act-cell:handle-cast generic-function)
+  (act-cell:pre-start generic-function)
+  (act-cell:after-stop generic-function)
+  (act-cell:stop generic-function)
+  (act-cell:call function)
+  (act-cell:cast function)
+  (act-cell:running-p function))
+
 (in-package :act)
 (pax:defsection @actor (:title "Actor")
-  "This is a description for the actor api."
   (act:actor class)
   (act:make-actor generic-function)
   (act:actor-of pax:macro)
@@ -50,11 +51,20 @@
   (act:path generic-function)
   (act:watch generic-function)
   (act:unwatch generic-function)
-  (act:watchers generic-function))
+  (act:watchers generic-function)
+
+  (act-cell:@actor-cell pax:section))
+
+(in-package :agt)
+(pax:defsection @agent (:title "Agent")
+  (agt:agent class)
+  (agt:make-agent function)
+  (agt:agent-get function)
+  (agt:agent-update function)
+  (agt:agent-stop function))
 
 (in-package :disp)
 (pax:defsection @dispatcher (:title "Dispatcher")
-  "This is a description for the dispatcher api."
   (disp:dispatcher-base class)
   (disp:shared-dispatcher class)
   (disp:make-dispatcher function)
@@ -67,7 +77,6 @@
 
 (in-package :router)
 (pax:defsection @router (:title "Router")
-  "This is a description for the router api."
   (router:router class)
   (router:make-router function)
   (router:add-routee function)
@@ -77,3 +86,19 @@
   (router:ask-s generic-function)
   (router:ask generic-function))
 
+(defpackage :cl-gserver.docs)
+(in-package :cl-gserver.docs)
+
+(pax:defsection @cl-gserver (:title "cl-gserver documentation")
+  "The main documentation of cl-gserver
+
+Please see this [Introduction](https://github.com/mdbergmann/cl-gserver/blob/master/README.md)
+for a basic walk through."
+
+  (asys:@actor-system pax:section)
+  (ac:@actor-context pax:section)
+  (act:@actor pax:section)
+  (agt:@agent pax:section)
+  (disp:@dispatcher pax:section)
+  (router:@router pax:section)
+  )

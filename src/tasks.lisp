c0827c8dad9bd86eb90097dbdf9cb54b5f65b268
Manfred Bergmann <mb@software-by-mabe.com>
2021-05-25 20:06:04 +0200

    added dedicated 'task' actor sub class. added task-shutdown. added task-async-stream.

--- src/tasks.lisp
+++ src/tasks.lisp
@@ -1,11 +1,16 @@
 (defpackage :cl-gserver.tasks
   (:use :cl)
   (:nicknames :tasks)
+  (:import-from #:binding-arrows
+                #:->>)
   (:export #:with-context
+           #:task
            #:task-yield
            #:task-start
            #:task-async
-           #:task-await)
+           #:task-await
+           #:task-shutdown
+           #:task-async-stream)
   )
 
 (in-package :cl-gserver.tasks)
@@ -13,11 +18,31 @@
 (defvar *task-context*)
 
 (defmacro with-context (context &body body)
+  "`with-context` creates an environment where the `tasks` package functions should be used in.
+`context` can be either an `asys:actor-system`, an `ac:actor-context`, or an `act:actor` (or subclass).
+The tasks created using the `tasks` functions will then be created in the given context.
+
+Example:
+
+```elisp
+;; create actor-system
+(defparameter *sys* (make-actor-system))
+
+(with-context *sys*
+  (task-yield (lambda () (+ 1 1))))
+=> 2 (2 bits, #x2, #o2, #b10)
+```
+"
   `(let ((*task-context* ,context))
      ,@body))
 
-(defun make-tmp-actor (context)
+(defclass task (act:actor) ()
+  (:documentation
+   "A dedicated `act:actor` subclass used for tasks."))
+
+(defun make-task (context)
   (act:actor-of (context)
+    :type 'task
     :receive (lambda (self msg state)
                (declare (ignore self))
                (cond
@@ -29,38 +54,103 @@
                  (t (cons :unrecognized-command state))))))
 
 (defun task-yield (fun &optional time-out)
-  "`task-yield` runs the given function `fun` in a temporary spawned actor by blocking and waiting for a response from the actor, or until the given timeout was elapsed.
+  "`task-yield` runs the given function `fun` by blocking and waiting for a response from the `task`, or until the given timeout was elapsed.
+`fun` must be a 0-arity function.
 
 A normal response from the actor is passed back as the response value.
-If the timeout elapsed the response is: `(cons :handler-error utils:ask-timeout)`."
-  (let ((tmp-actor (make-tmp-actor *task-context*)))
-    (prog1
-        (act:ask-s tmp-actor (cons :exec fun) :time-out time-out)
-      (ac:stop *task-context* tmp-actor))))
+If the timeout elapsed the response is: `(values :handler-error utils:ask-timeout)`.
+
+Example:
+
+```elisp
+;; create actor-system
+(defparameter *sys* (make-actor-system))
+
+(with-context *sys*
+  (task-yield (lambda () (+ 1 1))))
+=> 2 (2 bits, #x2, #o2, #b10)
+```
+"
+  (let ((task (make-task *task-context*)))
+    (unwind-protect
+         (let ((ask-result (act:ask-s task (cons :exec fun) :time-out time-out)))
+           (cond
+             ((consp ask-result)
+              (values (car ask-result) (cdr ask-result)))
+             (t ask-result)))
+      (ac:stop *task-context* task))))
 
 (defun task-start (fun)
-  "`task-start` runs the given function `fun` asynchronously in a temporary actor.
+  "`task-start` runs the given function `fun` asynchronously.
+`fun` must be a 0-arity function.
 Use this if you don't care about any response or result, i.e. for I/O side-effects.
-It returns `(values :ok <actor-ref>)`. `<actor-ref> is the actor given back as reference.
-The temporary actor is automatically stopped and removed from the context and will not be able to handle requests."
-  (let ((tmp-actor (make-tmp-actor *task-context*)))
+It returns `(values :ok <task>)`. `<task> is in fact an actor given back as reference.
+The task is automatically stopped and removed from the context and will not be able to handle requests."
+  (let ((task (make-task *task-context*)))
     (unwind-protect
          (progn
-           (act:tell tmp-actor (cons :exec fun))
-           (values :ok tmp-actor))
-      (ac:stop *task-context* tmp-actor))))
+           (act:tell task (cons :exec fun))
+           (values :ok task))
+      (ac:stop *task-context* task))))
 
 (defun task-async (fun)
-  "`task-async` schedules the function `fun` for asynchronous execution in a temporary spawned actor.
-The result of this function is the temporary spawned actor as 'task'.
-Store this result for a call to `task-async`."
-  (let ((tmp-actor (make-tmp-actor *task-context*)))
-    (act:tell tmp-actor (cons :exec fun))
-    tmp-actor))
+  "`task-async` schedules the function `fun` for asynchronous execution.
+`fun` must be a 0-arity function.
+The result of `task-async` is a `task`.
+Store this `task` for a call to `task-async`.
+Users must call either `task-await` or `task-shutdown` for the task to be cleaned up.
 
-(defun task-await (task-actor)
-  "`task-await` waits (by blocking) until a result has been generated for a previous `task-async` by passing the 'task' result of `task-async` (which is effectively an actor instance) to `task-await`.
-`task-await` also stops the temporary actor that is the result of `task-async`, so it is of no further use."
+Example:
+
+```elisp
+;; create actor-system
+(defparameter *sys* (make-actor-system))
+
+(with-context *sys*
+  (let ((x (task-async (lambda () (some bigger computation))))
+        (y 1))
+    (+ (task-await x) y)))
+```
+"
+  (let ((task (make-task *task-context*)))
+    (act:tell task (cons :exec fun))
+    task))
+
+(defun task-await (task)
+  "`task-await` waits (by blocking) until a result has been generated for a previous `task-async` by passing the `task` result of `task-async` to `task-await`.
+`task-await` also stops the `task` that is the result of `task-async`, so it is of no further use."
   (prog1
-    (act:ask-s task-actor :get)
-    (ac:stop *task-context* task-actor)))
+    (act:ask-s task :get)
+    (ac:stop *task-context* task)))
+
+(defun task-shutdown (task)
+  "`task-shutdown` shuts down a task in order to clean up resources."
+  (ac:stop *task-context* task))
+
+(defun task-async-stream (fun lst)
+  "`task-async-stream` concurrently applies `fun` on all elements of `lst`.
+`fun` must be a one-arity function taking an element of `lst`.
+
+The concurrency depends on the number of available `:shared` dispatcher workers.
+Each element of `lst` is processed by a worker of the `asys:actor-system`s `:shared` dispatcher worker.
+If all workers are busy then the computation of `fun` is queued.
+
+Example:
+
+```elisp
+;; create actor-system
+(defparameter *sys* (make-actor-system))
+
+(with-context *sys*
+  (->> 
+    '(1 2 3 4 5)
+    (task-async-stream #'1+)
+    (reduce #'+)))
+
+=> 20 (5 bits, #x14, #o24, #b10100)
+```
+"
+  (->>
+    lst
+    (mapcar (lambda (x) (task-async (lambda () (funcall fun x)))))
+    (mapcar #'task-await)))
--- tests/tasks-test.lisp
+++ tests/tasks-test.lisp
@@ -30,30 +30,31 @@
                    (mapcar (lambda (x)
                              (task-yield (lambda () (* 2 x))))
                            '(1 2 3 4))))
-        (let ((timed-result (task-yield (lambda () (sleep .5)) 0.2)))
-          (is (eq :handler-error (car timed-result)))
-          (is (typep (cdr timed-result) 'ask-timeout))))))
+        (multiple-value-bind (result condition)
+            (task-yield (lambda () (sleep .5)) 0.2)
+          (format t "Cond: ~a~%" condition)
+          (is (eq :handler-error result))
+          (is-true (typep condition 'ask-timeout))))))
 
 (test task-start
   "Test for task-start"
   (with-fixture system-fixture ()
     (with-context system
       (let ((my-var nil))
-        (multiple-value-bind (result actor)
+        (multiple-value-bind (result task)
             (task-start (lambda () (setf my-var 10)))
           (is (eq :ok result))
-          (is-true (typep actor 'act:actor))
+          (is-true (typep task 'tasks:task))
           (is-true (assert-cond (lambda () (= 10 my-var)) 0.2))
-          (is (eq :stopped (act:ask-s actor :foo))))))))
+          (is (eq :stopped (act:ask-s task :foo))))))))
 
 (test task-async
   "Test for task-async."
   (with-fixture system-fixture ()
     (with-context system
       (let ((task (task-async (lambda () (+ 1 2)))))
-        (is-true (typep task 'act:actor))
-        ;; explicitly stop. since we still need the actor for a potential `await'.
-        (ac:stop system task)))))
+        (is-true (typep task 'tasks:task))
+        (task-shutdown task)))))
 
 (test task-async--with-await
   "Test for task-async followed by task-await."
@@ -68,3 +69,14 @@
     (with-context system
       (let ((task (task-async (lambda () (sleep 1) (+ 1 2)))))
         (is (= 3 (task-await task)))))))
+
+(test task-async-stream
+  "Tests for task-async-stream"
+  (with-fixture system-fixture ()
+    (with-context system
+      (is (equal '(2 4 6 8 10)
+                 (task-async-stream (lambda (x) (* x 2))
+                                    '(1 2 3 4 5))))
+      (is (= 30 (reduce #'+
+                        (task-async-stream (lambda (x) (* x 2))
+                                           '(1 2 3 4 5))))))))

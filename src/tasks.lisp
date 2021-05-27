402dc93847b7793b38cbb575e3ef4c9c76ce69d5
Manfred Bergmann <mb@software-by-mabe.com>
2021-05-27 14:50:15 +0200

    documentation updates.

--- src/tasks.lisp
+++ src/tasks.lisp
@@ -36,7 +36,7 @@ Example:
 
 Since the default `:shared` dispatcher should mainly be used for the message dispatching, 
 but not so much for longer running tasks it is possible to create an actor system with additional
-dispatchers. This additional dispatcher can be utilized for `tasks`.
+dispatchers. This additional dispatcher can be utilized for `tasks`. Be aware that the config as used below is merged with the `asys:*default-config*` which means that the dispatcher `:foo` here is really an additional dispatcher.
 
 ;; create actor-system with additional (custom) dispatcher
 (defparameter *sys* (asys:make-actor-system '(:dispatchers (:foo (:workers 16)))))

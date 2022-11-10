915895fccfd2dc75a7ba496eef1e95e39ea940f9
Manfred Bergmann <mb@software-by-mabe.com>
2022-11-10 19:24:59 +0100

    mapping code for futures.

--- src/fcomputation.lisp
+++ src/fcomputation.lisp
@@ -5,7 +5,8 @@
            #:make-future
            #:complete-p
            #:on-completed
-           #:get-result))
+           #:get-result
+           #:fmap))
 
 (in-package :sento.future)
 
@@ -24,12 +25,12 @@ The `future` is used as part of `act:ask` but is available as a general utility.
     (with-slots (promise) obj
       (format stream "promise: ~a" promise))))
 
-(defun make-future (execute-fun)
-  "Creates a future. `execute-fun` is the lambda that is executed when the future is created.
-`execute-fun` takes a parameter which is the `resolve-fun` funtion. `resolve-fun` function
+(defun make-future (resolve-fun)
+  "Creates a future. `resolve-fun` is the lambda that is executed when the future is created.
+`resolve-fun` takes a parameter which is the `resolve-fun` funtion. `resolve-fun` function
 takes the `promise` as parameter which is the computed value. Calling `resolve-fun` with the promise
 will fulfill the `future`.  
-Manually calling `resolve-fun` to fulfill the `future` is in contrast to just fulfill the `future` from a return value. The benefit of the `resolve-fun` is flexibility. In  a multi-threaded environment `execute-fun` could spawn a thread, in which case `execute-fun` would return immediately but no promise can be given at that time. The `resolve-fun` can be called from a thread and provide the promise.
+Manually calling `resolve-fun` to fulfill the `future` is in contrast to just fulfill the `future` from a return value. The benefit of the `resolve-fun` is flexibility. In  a multi-threaded environment `resolve-fun` could spawn a thread, in which case `resolve-fun` would return immediately but no promise can be given at that time. The `resolve-fun` can be called from a thread and provide the promise.
 
 Create a future with:
 
@@ -46,7 +47,13 @@ Create a future with:
       (setf promise
             (create-promise (lambda (resolve-fn reject-fn)
                               (declare (ignore reject-fn))
-                              (funcall execute-fun resolve-fn)))))
+                              (funcall resolve-fun resolve-fn)))))
+    future))
+
+(defun make-future-plain (p)
+  (let ((future (make-instance 'future)))
+    (with-slots (promise) future
+      (setf promise p))
     future))
 
 (defun complete-p (future)
@@ -65,7 +72,43 @@ If the `future` is already complete then the `completed-fun` function is called 
 (defun get-result (future)
   "Get the computation result. If not yet available `:not-ready` is returned."
   (with-slots (promise) future
-    (with-slots (values) promise
-      (if (null values)
-          :not-ready
-          (car values)))))
+    (let ((the-promise (blackbird-base::lookup-forwarded-promise promise)))
+      (with-slots (values) the-promise
+        (if (null values)
+            :not-ready
+            (car values))))))
+
+(defun fmap (future map-fun)
+  (with-slots (promise) future
+    (let* ((the-promise (blackbird-base::lookup-forwarded-promise promise))
+           (cb-return-promise (blackbird-base::make-promise :name nil))
+           (cb-wrapped (lambda (&rest args)
+                         (blackbird-base::with-error-handling
+                             (errexit promise)
+                             (lambda (e)
+                               (blackbird-base::signal-error cb-return-promise e)
+                               (return-from errexit))
+                           (let ((cb-return (multiple-value-list (apply map-fun args))))
+                             ;; (format t "cb-return: ~a~%" cb-return)
+                             ;; the below is a special treatment to make this
+                             ;; work with out 'future'
+                             (let ((new-cb-return
+                                     (cond
+                                       ((typep (car cb-return) 'future)
+                                        (list (slot-value (car cb-return) 'promise)))
+                                       (t
+                                        cb-return))))
+                               (apply #'blackbird-base::finish
+                                      (append
+                                       (list cb-return-promise)
+                                       new-cb-return))))))))
+      (blackbird-base::attach-errback the-promise
+                                      (lambda (e)
+                                        (blackbird-base::signal-error
+                                         cb-return-promise
+                                         e)))
+      (blackbird-base::do-add-callback
+        the-promise
+        (blackbird-base::wrap-callback cb-wrapped))
+      (blackbird-base::run-promise the-promise)
+      (make-future-plain cb-return-promise))))
--- tests/fcomputation-test.lisp
+++ tests/fcomputation-test.lisp
@@ -17,22 +17,23 @@
   "Creates a future"
 
   (is (typep (make-future nil) 'future))
-  (is (typep (make-future (lambda (promise-fun)
-                            (declare (ignore promise-fun)) nil)) 'future)))
+  (is (typep (make-future (lambda (resolve-fun)
+                            (declare (ignore resolve-fun)) nil))
+             'future)))
 
 (test provide-promise
   "Executes future and provides promise"
 
-  (let ((future (make-future (lambda (promise-fun)
-                               (funcall promise-fun "fulfilled")))))
+  (let ((future (make-future (lambda (resolve-fun)
+                               (funcall resolve-fun "fulfilled")))))
     (is (eq t (complete-p future)))
     (is (string= "fulfilled" (get-result future)))))
 
 (test on-complete-callback
   "Executes future and get result via on-complete callback."
 
-  (let ((future (make-future (lambda (promise-fun)
-                               (funcall promise-fun "fulfilled"))))
+  (let ((future (make-future (lambda (resolve-fun)
+                               (funcall resolve-fun "fulfilled"))))
         (completed-value nil))
     (on-completed future (lambda (value) (setf completed-value value)))
     (is (string= "fulfilled" completed-value))))
@@ -40,19 +41,29 @@
 (test complete-with-delay
   "Test the completion with on-completed callback with a delayed execution."
 
-  (let ((future (make-future (lambda (promise-fun)
+  (let ((future (make-future (lambda (resolve-fun)
                                (bt:make-thread (lambda ()
                                                  (sleep 0.5)
-                                                 (funcall promise-fun "fulfilled"))))))
+                                                 (funcall resolve-fun "fulfilled"))))))
         (completed-value))
     (is (eq :not-ready (get-result future)))
     (on-completed future (lambda (value) (setf completed-value value)))
-    (is (eq t (assert-cond (lambda () (string= "fulfilled" completed-value)) 1)))
-    )
-  )
+    (is (eq t (assert-cond (lambda () (string= "fulfilled" completed-value)) 1)))))
 
-(defun run-tests ()
-  (run! 'create-future)
-  (run! 'provide-promise)
-  (run! 'on-complete-callback)
-  (run! 'complete-with-delay))
+(test mapping-futures
+  "Tests mapping futures"
+  (flet ((future-generator (x)
+           (make-future (lambda (resolve-fun)
+                          (funcall resolve-fun (+ x 1))))))
+    (let ((future (fmap (future-generator 0)
+                        (lambda (completed-value)
+                          (fmap (future-generator completed-value)
+                                (lambda (completed-value)
+                                  (fmap (future-generator completed-value)
+                                        (lambda (completed-value)
+                                          completed-value))))))))
+      (is-true (assert-cond (lambda ()
+                              (= 3 (get-result future)))
+                            1)))))
+
+(run! 'mapping-future)

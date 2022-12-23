6da42706504ccc7380e98cff1760217f13e39c39
Manfred Bergmann <mb@software-by-mabe.com>
2022-12-23 21:01:07 +0100

    first test for stashing

---
+++ src/stash.lisp
@@ -0,0 +1,10 @@
+(defpackage :sento.stash
+  (:use :cl)
+  (:nicknames :stash)
+  (:export #:stash)
+  )
+
+(in-package :sento.stash)
+
+(defclass stash () ()
+  (:documentation ""))
---
+++ tests/stash-test.lisp
@@ -0,0 +1,30 @@
+(defpackage :sento.stash-test
+  (:use :cl :fiveam :sento.stash)
+  (:export #:run!
+           #:all-tests
+           #:nil))
+(in-package :sento.stash-test)
+
+(def-suite stash-tests
+  :description "Tests for stash mixin"
+  :in sento.tests:test-suite)
+
+(in-suite stash-tests)
+
+(def-fixture test-context ()
+  (let ((system (asys:make-actor-system '(:dispatchers (:shared (:workers 1))))))
+    (unwind-protect
+         (&body)
+      (ac:shutdown system :wait t))))
+
+(defclass stash-actor (act:actor stash) ())
+
+(test create-actor-with-stash
+  (with-fixture test-context ()
+    (is (not (null (ac:actor-of system
+                                :type 'stash-actor
+                                :receive (lambda (self msg state)
+                                           (descare (ignore self msg state)))))))))
+
+
+(run! 'create-actor-with-stash)
